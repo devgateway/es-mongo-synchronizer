390a257b4a4e1d7031d39595e25965e7a84455da
Sebastian Dimunzio <sdimunzio@gmail.com>
2015-09-22 23:18:44 -0300

    adding time out and reducing number of active calls

--- conf.js
+++ conf.js
@@ -3,7 +3,8 @@ module.exports = {
 	"oplog_db": "//127.0.0.1:27017/local",
 	"es": "localhost:9200",
 	'log': 'info',
-	'max_active_request':300, //how many request to elastic search can be active 
+	'max_active_request':25, //how many request to elastic search can be active ,
+	 'requestTimeout': 60000,
 	/*namespace should be database.collection*/
 	'ns_mapping': {
 		'aiddata.project': {
