0fd0c5cd5da5d62bb62e1bf3bb73016f996b0d0e
Sebastian Dimunzio <sdimunzio@gmail.com>
2015-09-29 23:41:59 -0300

    adding an space!

--- app.js
+++ app.js
@@ -307,7 +307,7 @@
 
 
  oplog.on('op', function(data) {
-   logWithDate('Last ts  readed for' + data.ns + 'is ' + data.ts);
+   logWithDate('Last ts  readed for ' + data.ns + 'is ' + data.ts);
  });
 
 
